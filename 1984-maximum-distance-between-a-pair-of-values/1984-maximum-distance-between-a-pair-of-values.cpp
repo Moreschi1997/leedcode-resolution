2164ae80cb064913fbab4b8a35012976e0534016
Moreschi <luizfellipe1997.29@gmail.com>
2026-04-19 18:55:08 -0300

    1984-MAXIMUM-DISTANCE-BETWEEN-A-PAIR-OF-VALUES Stats: Time: 0 ms (100%), Space: 102.3 MB (10.83%) - LeetHub

--- 1984-maximum-distance-between-a-pair-of-values/1984-maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/1984-maximum-distance-between-a-pair-of-values.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
+        
         int n = nums1.size(), m = nums2.size();
         int maxDist = 0;
-        while(i < n && j < m)
+        for(int i = 0, j = 0; i < n && j < m; j++)
         {
           if(nums1[i] <= nums2[j])
           {
@@ -13,7 +13,6 @@ public:
             {
                 maxDist = currentDist;
             }
-            j++;
           }
           else
           {
